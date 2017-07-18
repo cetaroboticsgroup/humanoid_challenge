4c0a78577f990f8c775847bc23cc8d2b8fe2ec92
Monsoon117 <monsoon117@gmail.com>
2017-07-18 13:48:30 -0400

    Add files via upload

--- singleJointControl.ino
+++ singleJointControl.ino
@@ -15,15 +15,26 @@ int d (float input){
  return (output);
 }
 
+
 //Single Joint Control function
-void SJC(int ID,int GP,int T)//ID num, Goal postion, Time
+void SJC(int ID,int GP,int T)//ID num, Goal postion, Time(ms)
 {
   int isMoving = Dxl.readByte(ID, 49);
   if( isMoving == 0 ){  // move or not
-    int NP = Dxl.readWord(ID,37); // now postion
-    int speed = (NP-GP)/T;  //caculate speed
-    Dxl.setPosition(ID,GP,speed); // wirte word to motor
+    int PP = Dxl.readWord(ID,37); // present postion
+    int speed;
+    if(GP>=GP){
+      speed = (GP-PP)*1000/T/2.2867; //unit is about 0.111rpm = 0.67deg/s = 2.2867/s 
+    }
+    else{
+      speed = (PP-GP)*1000/T/2.2867;
+      speed = speed +1023;}
+    Dxl.writeWord(ID,32,speed) ; // wirte word to motor
+    Dxl.writeWord(ID,30,GP) ; 
+    T = T +100;
     delay(T);
+    SerialUSB.print("Present speed");
+    SerialUSB.println(speed);
   }
 }
 
@@ -37,6 +48,6 @@ void setup() {
 
 
 void loop(){
-SJC(5,d(-90),600); // call singel joint control function :ID,angle,time
-SJC(5,d(60),300);
+SJC(5,d(-90),100); // call singel joint control function :ID,angle,time
+SJC(5,d(0),300);
 }
