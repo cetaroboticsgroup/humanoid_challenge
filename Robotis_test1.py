8a0879c3fea0a75484931febcc2f5c3c48725b6a
shazib2t <shazib2t@gmail.com>
2018-03-05 14:27:52 -0500

    Add files via upload

---
+++ Robotis_test1.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import serial
+
+connected = True
+
+ser = serial.Serial("/dev/ttyACM0", 9600)
+
+while not connected:
+    serin = ser.read()
+    connected = True
+
+ser.write("1")
+ser.read()
+ser.close()
