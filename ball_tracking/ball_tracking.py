5f6c02dbfe140e39d692cb9d351b4c4d8d68c3d7
shazib2t <shazib2t@gmail.com>
2018-03-22 00:06:42 -0400

    Update ball_tracking.py

--- ball_tracking/ball_tracking.py
+++ ball_tracking/ball_tracking.py
@@ -59,7 +59,7 @@ while True:
 				(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-		print("found one circle")
+		#print("found one circle")
 		ser.write("1")
 
 	# show the frame to our screen
